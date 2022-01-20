992c249b92260fccf68f377970611fecbde959d3
vinicius <viniholtman28@gmail.com>
2022-01-20 11:35:23 -0300

    Create model porta

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Porta from "../components/Porta";
+import PortaModel from "../model/PortaModel";
 
 
 
 export default function Home() {
+  const [p1, setp1] = useState(new PortaModel(1))
+
   return (
-    <div>
-      <Porta selecionada/>
+    <div style={{display: "flex"}}>
+      <Porta porta={p1}/>
     </div>
   )
 }
