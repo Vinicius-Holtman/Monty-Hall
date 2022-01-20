cdf6c3028eb7cf4ae82d0aca5f889356c753a9bf
vinicius <viniholtman28@gmail.com>
2022-01-20 09:57:09 -0300

    Create components Porta and Presente

---
+++ components/Porta.jsx
@@ -0,0 +1,9 @@
+import styles from '../styles/Porta.module.css'
+
+export default function Porta() {
+    return (
+        <div className={styles.porta}>
+            <div className={styles.numero}></div>
+        </div>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Porta from "../components/Porta";
+
+
+
+export default function Home() {
+  return (
+    <div>
+      <Porta />
+    </div>
+  )
+}
