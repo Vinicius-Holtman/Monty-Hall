1187cfdb5ab643c554ec02a746c586329423e6e0
vinicius <viniholtman28@gmail.com>
2022-01-21 11:39:29 -0300

    Created router link

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,19 +6,9 @@ import PortaModel from "../model/PortaModel";
 
 
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(3, 2))
-
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return <Porta key={porta.numero} value={porta} onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}></Porta>
-    })
-  }
-
-  console.log(criarPortas(3, 2))
-
   return (
     <div style={{display: "flex", flexDirection: "column"}}>
-      {renderizarPortas()}
+      <h2>formulario de inicio</h2>
     </div>
   )
 }
